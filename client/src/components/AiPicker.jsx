de334d65c7ea6b033422de61376f2ef5b54139e1
LucasNF22 <lucasnfiorentino@gmail.com>
2023-09-20 20:18:15 -0300

    Configuracion de server backend (errores), configuracion de aipicker

--- client/src/components/AiPicker.jsx
+++ client/src/components/AiPicker.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 
-const AiPicker = () => {
+import CustomButtom from './CustomButton'
+
+const AiPicker = ({ prompt, setPrompt, generatinImg, handleSubmit }) => {
   return (
-    <div>AiPicker</div>
+    <div className='aipicker-container'>
+        <textarea
+          placeholder='Pregunta a la IA'
+          rows={5}
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          className='aipicker-textarea'
+        />
+        <div className='flex flex-wrap gap-3'>
+          { generatinImg ? (
+            <CustomButtom
+              type="outline"
+              title="Consultando con la IA"
+              customStyles="text-xs"
+            />
+          ) : (
+            <>
+              <CustomButtom 
+                type="outline"
+                title="AI Logo"
+                handleClick={() => handleSubmit('logo') }
+                customStyles="text-xs"
+              />
+
+              <CustomButtom 
+                type="filled"
+                title="AI Full"
+                handleClick={() => handleSubmit('full') }
+                customStyles="text-xs"
+              />
+            </>
+          )
+
+          }
+        </div>
+    </div>
   )
 }
 
-export default AiPicker
+export default AiPicker;
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -55,10 +55,25 @@ const Customizer = () => {
   };
 
   const handleSubmit = async(type) => { 
-    if(!prompt) return alert("Please enter a prompt");
+    if(!prompt) return alert("Ingrese su pedido a la IA");
 
     try {
       // Llamar al backend para generar la imagen por IA
+      setGeneratingImg(true);
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
 
     } catch (error) {
       alert(error);
