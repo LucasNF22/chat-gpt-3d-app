c69b2a41c0a12b43ca28b1087c6f0256a6272957
LucasNF22 <lucasnfiorentino@gmail.com>
2023-09-21 13:59:57 -0300

    update de codigo en backend... openai consume creditos

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY 
 });
 
-const openai = new OpenAIApi(config);
+
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
@@ -20,14 +21,16 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.createImage({
+    const response = await openai.images.generate({
       prompt,
       n: 1,
       size: '1024x1024',
-      response_format: 'b64_json'
+      response_format: 'b64_json',
+      
     });
 
-    const image = response.data.data[0].b64_json;
+    console.log(response);
+    const image = response.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
